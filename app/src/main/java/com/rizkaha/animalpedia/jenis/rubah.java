c73666db1a3ffb2b5b5a4f6705e67fde0796c293
Rizka <rizkahra@gmail.com>
2020-11-19 17:33:37 +0800

    project pertama untuk percobaan

---
+++ app/src/main/java/com/rizkaha/animalpedia/jenis/rubah.java
@@ -0,0 +1,8 @@
+package com.rizkaha.animalpedia.jenis;
+
+public class rubah extends hewan {
+
+    public rubah(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Rubah",ras,asal,deskripsi,drawableRes);
+    }
+}
